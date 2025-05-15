d50e52c390d065a29f71ddbe34dc8a8910e0f4b6
Aakash tyagi <akashtyagi780251@gmail.com>
2025-05-15 23:53:39 +0530

    Your changes on frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 import AdminForm from './component/AdminForm'
 import Authorize from './component/Authorize'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
+
 function App() {
+  const [count, setCount] = useState(0)
+
   return (
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-    <>
-      {/* <Authorize/> */}
-      <Home/>
-    </>
-=======
     <div>
       <Routes>
         <Route path='/register' element={<Authorize />} />
@@ -19,15 +15,6 @@ function App() {
         <Route path='/' element={<Home />} />
       </Routes>
     </div>
->>>>>>> Stashed changes
-=======
-    <div>
-      <Routes>
-        <Route path='/register' element={<Authorize />} />
-        <Route path='/' element={<Home />} />
-      </Routes>
-    </div>
->>>>>>> main
   )
 }
 
